fdaa603e046f447ea7f4a98936f984aef2332a14
GlFranco04 <gabrielandreslopezfranco04@gmail.com>
2024-11-08 11:29:41 -0400

    Menus desplegables en el sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importar CommonModule para *ngIf
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  isSidebarOpen = true;
+  expandedMenus: { [key: string]: boolean } = {
+    gestionUsuario: false,
+  };
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen; // Alterna entre visible e invisible
+  }
+
+  toggleMenu(menu: string) {
+    this.expandedMenus[menu] = !this.expandedMenus[menu];
+  }
 }
