00086ce87e855365f16c57a37e134378a49c3b2f
Glfrancodev <gabrielandreslopezfrancodev@gmail.com>
2024-11-17 16:32:48 -0400

    Exportar Historia Clinica por personalizada posiblemente arreglado

--- src/app/core/services/pdf-export.service.ts
+++ src/app/core/services/pdf-export.service.ts
@@ -6,20 +6,22 @@ import autoTable from 'jspdf-autotable';
   providedIn: 'root',
 })
 export class PdfExportService {
+  constructor() {}
+
   exportToPDF(data: any[], columns: string[], title: string, fileName: string): void {
     const doc = new jsPDF();
+    doc.text(title, 10, 10);
 
-    // Título del PDF
-    doc.text(title, 14, 10);
+    // Encabezados como un array de strings
+    const headers = columns;
 
-    // Generar la tabla con autoTable
+    // Usar autoTable para renderizar los datos
     autoTable(doc, {
-      head: [columns],
-      body: data.map((item) => columns.map((col) => item[col] || '')), // Extraer solo los datos necesarios
-      startY: 20,
+      head: [headers], // Encabezados en un array de arrays
+      body: data.map((row) => headers.map((header) => row[header])), // Mapear datos según las columnas seleccionadas
+      startY: 20, // Ajustar para que no se sobreponga con el título
     });
 
-    // Descargar el archivo PDF
     doc.save(`${fileName}.pdf`);
   }
 }
